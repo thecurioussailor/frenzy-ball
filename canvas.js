9080eefe21ddc0c7d4af3ee18a38c5551f89fe66
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-07-24 23:00:37 +0530

    initial setup

---
+++ canvas.js
@@ -0,0 +1,15 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = 428;
+canvas.height = 540;
+
+var c = canvas.getContext("2d")
+
+c.fillStyle = "rgb(60, 179, 113, 0.5)"
+c.fillRect(1,1,60,60);
+c.fillRect(62,1,60,60);
+c.fillRect(123,1,60,60);
+c.fillRect(184,1,60,60);
+c.fillRect(245,1,60,60);
+c.fillRect(306,1,60,60);
+c.fillRect(367,1,60,60);
