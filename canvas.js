948cf3705b8e7c9e2c12dcf3704a5cdd75ba7a53
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-07-24 23:35:47 +0530

    green boxes

--- canvas.js
+++ canvas.js
@@ -5,11 +5,17 @@ canvas.height = 540;
 
 var c = canvas.getContext("2d")
 
-c.fillStyle = "rgb(60, 179, 113, 0.5)"
-c.fillRect(1,1,60,60);
-c.fillRect(62,1,60,60);
-c.fillRect(123,1,60,60);
-c.fillRect(184,1,60,60);
-c.fillRect(245,1,60,60);
-c.fillRect(306,1,60,60);
-c.fillRect(367,1,60,60);
+c.fillStyle = "rgb(60, 179, 113, 1)"
+c.fillRect(0,4,54,54);
+c.fillRect(58,4,54,54);
+c.fillRect(116,4,54,54);
+c.fillRect(174,4,54,54);
+c.fillRect(232,4,54,54);
+c.fillRect(290,4,54,54);
+c.fillRect(348,4,54,54);
+
+c.beginPath();
+c.arc(200,200, 9, 0, Math.PI * 2, false);
+c.strokeStyle = "white"
+c.stroke();
+
